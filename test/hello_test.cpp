bee4ea26f61f6bb478d2a59bdcb3c3d4b915152e
root <egor-medvedev00@mail.ru>
2023-09-17 17:46:29 +0000

    test

--- test/hello_test.cpp
+++ test/hello_test.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include <lib.h>
 
-TEST(ExampleOfstandartUsageTest1, BasicAssertions) {
+TEST(ExampleOfStandartUsageTest1, BasicAssertions) {
   // arrange 
   int first_number = 1;
   int second_number = 9;
@@ -12,7 +12,7 @@ TEST(ExampleOfstandartUsageTest1, BasicAssertions) {
   // assert
   EXPECT_EQ(res, 1);
 }
-TEST(ExampleOfstandartUsageTest2, BasicAssertions) {
+TEST(ExampleOfStandartUsageTest2, BasicAssertions) {
   // arrange 
   int first_number = 2;
   int second_number = 10;
@@ -23,7 +23,7 @@ TEST(ExampleOfstandartUsageTest2, BasicAssertions) {
   // assert
   EXPECT_EQ(res, 2);
 }
-TEST(ExampleOfstandartUsageTest1, BasicAssertions) {
+TEST(ExampleOfStandartUsageTest3, BasicAssertions) {
   // arrange 
   int first_number = 3;
   int second_number = 15;
