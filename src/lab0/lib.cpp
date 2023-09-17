c58b6333b87be83f78c69cbc759df66d0f6f9607
root <egor-medvedev00@mail.ru>
2023-09-17 17:29:31 +0000

    result

--- src/lab0/lib.cpp
+++ src/lab0/lib.cpp
@@ -4,5 +4,5 @@
 int gcd(int first_number,int second_number) {
     if (second_number==0)
         return first_number;
-    return gcd(b,a%b);
+    return gcd(second_number,first_number%second_number);
 }
