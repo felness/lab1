586efed8c349c4f9d6e93f9e9940a7128858062d
root <egor-medvedev00@mail.ru>
2023-09-17 17:42:31 +0000

    test

--- test/hello_test.cpp
+++ test/hello_test.cpp
@@ -1,14 +1,36 @@
 #include <gtest/gtest.h>
+#include <lib.h>
 
-
-TEST(HelloTest, BasicAssertions) {
+TEST(ExampleOfstandartUsageTest1, BasicAssertions) {
   // arrange 
-  int first = 2;
-  int second = 5;
+  int first_number = 1;
+  int second_number = 9;
 
   // act
-  int sum = first + second;
+  int res = gcd(first_number,second_number);
 
   // assert
-  EXPECT_EQ(sum, 7);
+  EXPECT_EQ(res, 1);
+}
+TEST(ExampleOfstandartUsageTest2, BasicAssertions) {
+  // arrange 
+  int first_number = 2;
+  int second_number = 10;
+
+  // act
+  int res = gcd(first_number,second_number);
+
+  // assert
+  EXPECT_EQ(res, 2);
+}
+TEST(ExampleOfstandartUsageTest1, BasicAssertions) {
+  // arrange 
+  int first_number = 3;
+  int second_number = 15;
+
+  // act
+  int res = gcd(first_number,second_number);
+
+  // assert
+  EXPECT_EQ(res, 3);
 }
