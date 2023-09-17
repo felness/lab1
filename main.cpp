0c16f02443a70bd3e4fe9e4444906767f913c863
root <egor-medvedev00@mail.ru>
2023-09-17 18:31:04 +0000

    result

--- main.cpp
+++ main.cpp
@@ -10,13 +10,7 @@ int main() {
     cin>>first;
     cout<<"Enter the second number:"<<endl;
     cin>>second;
-    if(gcd(first,second)<0) {
-         cout<<"gcd is "<<gcd(first,second)*-1<<endl;
-    }
-       
-    else{
-        cout<<"gcd is "<<gcd(first,second)<<endl;
-    }
+    cout<<"gcd is "<<gcd(first,second)<<endl;
     
 
 }
