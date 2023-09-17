ceb2d92afb84b125c7b39804f67c46b6f8fe343b
root <egor-medvedev00@mail.ru>
2023-09-17 18:30:42 +0000

    function

--- src/lab0/lib.cpp
+++ src/lab0/lib.cpp
@@ -1,7 +1,9 @@
-
+#include <cstdlib>
 #include "lib.h"
 
 int gcd(int first_number,int second_number) {
+    first_number=abs(first_number);
+    second_number=abs(second_number);
     if (second_number==0)
         return first_number;
     return gcd(second_number,first_number%second_number);
