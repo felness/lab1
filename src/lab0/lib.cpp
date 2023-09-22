fc208c322de9afcffd42356a004c9667976811db
felness <Nmedvedev85@mail.ru>
2023-09-23 02:21:56 +0300

    пробелы

--- src/lab0/lib.cpp
+++ src/lab0/lib.cpp
@@ -1,10 +1,10 @@
 #include <cstdlib>
 #include "lib.h"
 
-int gcd(int first_number,int second_number) {
-    first_number=abs(first_number);
-    second_number=abs(second_number);
+int gcd(int first_number, int second_number) {
+    first_number= abs (first_number);
+    second_number= abs (second_number);
     if (second_number==0)
         return first_number;
-    return gcd(second_number,first_number%second_number);
+    return gcd(second_number,first_number % second_number);
 }
